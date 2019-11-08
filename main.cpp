e618e77759fc4b1ac7dabcff2481e4ebc40166a4
Marius Hambro Bovbjerg <marius@hambro.eu>
2019-11-08 11:22:25 +0100

    commit 2

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * PRJ1-IKT5.cpp
+ *
+ * Created: 08/11/2019 11.10.41
+ * Author : Marius
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
